2f4992f5375b27e238328dc2c119010cb9976f58
2004Abhi <abgbjb2004@gmail.com>
2022-04-02 11:35:09 +0530

    commit 1

---
+++ inheritance/childclass.java
@@ -0,0 +1,21 @@
+package inheritance;
+//inheritance means a sub class aquires all the properties of a super class 
+//for this (extend)keyword is used
+class circle {
+    public double radius;
+    public circle() {
+        radius = 0.0;
+    }
+    public double area(double radius){
+        return Math.PI*radius;
+    }
+}
+class cylinder extends circle{
+    private double height;
+    public cylinder(){
+        height=0.0;
+    }
+    private double volume(double height){
+        return area(super.radius)*height;
+    }
+}
